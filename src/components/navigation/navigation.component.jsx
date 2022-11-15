98f42c5fb08a56f9cb0a808ae2233ef18cdfd3df
Uchechukwu Azubuine <uchechukwuazubuine@gmail.com>
2022-11-16 00:06:55 +0100

    navigation corrections

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -24,7 +24,7 @@ const Navigation = () => {
 
   return (
     <div className="container nav-container">
-      <header className="header nav-open">
+      <header className="header ">
         <Link to="/">
           <MetaLogo />
         </Link>
